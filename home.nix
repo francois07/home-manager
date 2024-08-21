7b63182a2c0202dbbf7525787eacbec07c532caf
francois07 <mtfrancois.soulie@gmail.com>
2024-08-21 19:52:14 +0200

    add: latex editing tools

--- home.nix
+++ home.nix
@@ -5,11 +5,16 @@
     modules/tmux
     modules/nvim
     modules/fonts
+    modules/latex
   ];
 
   home.username = "francois";
   home.homeDirectory = "/home/francois";
   home.stateVersion = "24.05";
+  home.sessionVariables = {
+    EDITOR = "nvim";
+    VISUAL = "nvim";
+  };
 
   programs.home-manager.enable = true;
 }
